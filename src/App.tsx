e94ebf4b33ad801a1772b0c98bf9b188f9db6a83
yyc <yenyu335@gmail.com>
2025-05-22 22:01:53 +0800

    fix dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import DestinationPage from "./pages/Destination";
-import CrewPage from "./pages/Crew";
-import TechnologyPage from "./pages/Technology";
+
 import NavBar from "./components/NavBar";
 import AnimatedOutlet from "./components/AnimatedOutlet";
 import "./App.css";
 // bg-[#0B0D17]
 function App() {
   return (
-    <Router>
-      <div className="relative">
-        <NavBar />
-        <AnimatedOutlet />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<DestinationPage />} />
-          <Route path="/crew" element={<CrewPage />} />
-          <Route path="/technology" element={<TechnologyPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="bg-[#0B0D17] overflow-x-hidden absolute w-full h-full">
+      <NavBar />
+      <AnimatedOutlet />
+    </div>
   );
 }
 
